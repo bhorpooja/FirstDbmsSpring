3138491cbb18e1aa88fcdfb58ab2e3b732756af2
pooja <bhorpooja222gmail.com>
2017-10-26 14:05:20 +0530

    Initial commit

---
+++ src/main/java/com/codekul/FirstDbmsSpring/FirstDbmsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.codekul.FirstDbmsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDbmsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDbmsSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codekul/FirstDbmsSpring/FirstDbmsSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codekul.FirstDbmsSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstDbmsSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
